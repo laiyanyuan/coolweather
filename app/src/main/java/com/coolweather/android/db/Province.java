355fd80fcd0b78432381ed52c20683078c8ee1f1
lai yanyuan <1755576248@qq.com>
2020-12-11 16:16:53 +0800

    加入数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,31 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2020/12/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
